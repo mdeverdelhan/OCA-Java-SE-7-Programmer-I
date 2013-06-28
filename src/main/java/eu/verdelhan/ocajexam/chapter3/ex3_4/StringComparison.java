7a41c45c3da5575138fc1d1eea63d52b9ec97f6f
mdeverdelhan <marc.deverdelhan@yahoo.com>
2013-06-28 19:38:42 +0200

    Completing exercise 3-4

--- src/main/java/eu/verdelhan/ocajexam/chapter3/ex3_4/StringComparison.java
+++ src/main/java/eu/verdelhan/ocajexam/chapter3/ex3_4/StringComparison.java
@@ -1,14 +1,27 @@
 package eu.verdelhan.ocajexam.chapter3.ex3_4;
 
+/**
+ * See the documentation of the compareTo(String anotherString) method of the String class:
+ * http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#compareTo(java.lang.String)
+ */
 public class StringComparison {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+        
         String eggs1 = "Cackle fruit";
         String eggs2 = "Cackle fruit";
         System.out.println(eggs1.compareTo(eggs2));
+        // 0 is printed because eggs1.equals(eggs2).
+
+        eggs1 = "A fruit";
+        eggs2 = "The fruit";
+        System.out.println(eggs1.compareTo(eggs2));
+        // A negative integer is printed because eggs1 lexicographically precedes eggs2.
+
+        eggs1 = "Your fruit";
+        eggs2 = "My fruit";
+        System.out.println(eggs1.compareTo(eggs2));
+        // A positive integer is printed because eggs1 lexicographically follows eggs2.
     }
 
 }
