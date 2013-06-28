450fff34c9721f67f0755bfdf202ff1c6ddc2aba
mdeverdelhan <marc.deverdelhan@yahoo.com>
2013-06-28 19:22:33 +0200

    Completing exercise 3-2

--- src/main/java/eu/verdelhan/ocajexam/chapter3/ex3_2/StringTest.java
+++ src/main/java/eu/verdelhan/ocajexam/chapter3/ex3_2/StringTest.java
@@ -1,9 +1,15 @@
 package eu.verdelhan.ocajexam.chapter3.ex3_2;
 
+/**
+ * Official FindBugs™ website: http://findbugs.sourceforge.net/
+ */
 public class StringTest {
 
     public static void main(String[] args) {
-        String s1 = new String("String one");
+        // Fixing the performance bug (found with FindBugs™)
+//      String s1 = new String("String one");
+        String s1 = "String one";
+        
         String s2 = "String two";
         String s3 = "String " + "three";
     }
